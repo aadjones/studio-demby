e5531bc61f497166436672ddfd33066e0b3a0678
aadjones <aaron.demby.jones@gmail.com>
2025-04-05 18:04:08 -0700

    Update ZoomImage to be more autoscaling

--- app/components/ZoomImage.tsx
+++ app/components/ZoomImage.tsx
@@ -5,7 +5,8 @@ import 'react-medium-image-zoom/dist/styles.css';
 export default function ZoomImage({ src, alt }: { src: string; alt: string }) {
   return (
     <Zoom>
-      <img src={src} alt={alt} className="rounded-lg shadow cursor-zoom-in" />
+      <img src={src} alt={alt}   className="w-full h-auto rounded-lg shadow cursor-zoom-in object-cover"
+ />
     </Zoom>
   );
 }
